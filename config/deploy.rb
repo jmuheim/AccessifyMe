fa515773d98b16c33a52ebe4fc44397441d2220d
Josua Muheim <>
2022-10-05 09:24:03 +0200

    prototyping

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:jmuheim/AccessifyMe.git"
 set :branch, `git rev-parse --abbrev-ref HEAD`.chomp # Simply use the current branch!
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/virtual/www/xsfi/rails"
+set :deploy_to, "/var/www/virtual/xsfi/rails"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
